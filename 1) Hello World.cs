956c3bdacbc48fefeea9450a0d7699e7b8b3a025
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-10-19 00:05:25 +0100

    Add files via upload

---
+++ 1) Hello World.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MIkeDaneTutorialCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, there is a new C# Dev in the building. ");
+            Console.ReadLine();
+        }
+    }
+}
