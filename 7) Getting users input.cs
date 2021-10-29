c1b040f45afc68d6b388f6d65351411f2524d9eb
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-10-29 01:05:06 +0100

    Add files via upload

---
+++ 7) Getting users input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GettingUsersInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("You are welcome, oh " + name + ".");
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("You are welcome, oh " + name + " and you are " + age + " years old.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 8) Building a calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BuildingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("85" + "31"); // The output of this will be 8531.
+
+
+            int num = Convert.ToInt32("52");
+            Console.WriteLine(num + 46);
+
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 * num2);
+
+            Console.Write("Enter a number: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num3 / num4);
+
+            Console.ReadLine();
+        }
+    }
+}
