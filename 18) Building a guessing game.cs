21fc8ef013eedca5bffea80f98eeae764c40c251
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-11-05 06:36:31 +0100

    Add files via upload

---
+++ 18) Building a guessing game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Building_a_guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "King coder";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                Console.WriteLine("Enter a guess: ");
+                guess = Console.ReadLine();
+                guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("");
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.Write("You win!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 19) For loops.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace For_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            for (int i = 1; i <= 8; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int[] luckyNumbers = { 1, 5, 45, 15, 4, 15, 3, 8 };
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
