51cc59bc0182797ff7cd41c0d30c1c5ac747b6bf
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-11-14 00:51:19 +0100

    Add files via upload

---
+++ 29) Static Methods and Classes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Static_Methods_and_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+
+            UsefulTools.SayHi("Thomas");
+            Console.ReadLine();
+
+        }
+    }
+}
